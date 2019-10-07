3a60590db1861f2123f76e3973216ddb96ec4e28
Josue Carranza <josca0221@gmail.com>
2019-10-07 12:04:47 -0500

    Ultimos cambios

--- Tienda/routes/api.php
+++ Tienda/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Producto;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,29 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('productos/{producto}', function ($productId) {
+    return response()->json(['productId' => "{$productId}"], 200);
+});
+  
+ 
+Route::post('productos', function(Request $request) {
+    $resp = Producto::create($request->all());
+     return $resp;
+  
+ });
+ 
+Route::put('productos/{producto}', function() {
+    $product = Producto::findOrFail($productId);
+    $product->update($request->all());
+    return $product;
+});
+ 
+Route::get('producto/{producto}',function($producto) {
+    Producto::destroy($producto);
+    return 'User  deleted';
+});
+Route::get('productos', function () {
+    return response(Producto::all(),200);
+});
+
+
